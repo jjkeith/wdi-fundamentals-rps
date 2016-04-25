110bc219412bd392aba976caf562dbf793587c03
jjkeith <jjkeithoski@gmail.com>
2016-04-25 15:49:24 -0700

    Improved functionality and response

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,11 +35,9 @@ function getWinner(playerMove,computerMove) {
     var winner;
     if (playerMove === computerMove) {
       winner = 'tie';
-    } else if (playerMove === 'rock' && computerMove === 'scissors') {
-      winner = 'player';
-    } else if (playerMove === 'scissors' && computerMove === 'paper') {
-      winner = 'player';
-    } else if (playerMove === 'paper' && computerMove === 'rock') {
+    } else if ((playerMove === 'rock' && computerMove === 'scissors') 
+        || (playerMove === 'scissors' && computerMove === 'paper') 
+        || (playerMove === 'paper' && computerMove === 'rock')) {
       winner = 'player';
     } else {
       winner = 'computer';
@@ -54,13 +52,23 @@ function playToFive() {
     while (playerWins < 5 && computerWins < 5) {
         var playerMove = getPlayerMove();
         var computerMove = getComputerMove();
-        console.log("Player chose " + playerMove + " while Computer chose " + computerMove + ". " + getWinner(playerMove,computerMove) + " won!")
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove + ".")
       if (getWinner(playerMove,computerMove) === 'player') {
         playerWins += 1;
+        console.log("You won!");
       } else if (getWinner(playerMove,computerMove) === 'computer') {
         computerWins += 1;
+        console.log("You lost.");
+      } else {
+        console.log("It's a tie.");
       }
     }
+    if (computerWins === 5) {
+        console.log("The computer beat you.")
+    } else if (playerWins === 5) {
+        console.log("You beat the computer!")
+    }
+    
     return [playerWins, computerWins];
 }
 playToFive();
